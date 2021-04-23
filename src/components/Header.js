4c87bd1305d3a8396732888285a88572a0ba1f3e
sossw1 <sossw1@gmail.com>
2021-04-22 20:56:02 -0400

    Header parallax image and title

---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import headerImage from '../images/tech-background.jpg';
+
+function Header() {
+  return (
+    <div class="parallax-container">
+      <div class="parallax">
+        <img src={headerImage} alt="Tech background"/>
+      </div>
+      <h1 class="center-align white-text">William Soss</h1>
+      <h4 class="center-align white-text">Full Stack Web Developer</h4>
+    </div>
+  );
+}
+
+export default Header;
