924ace56bc12cf66f5bc82d5e6e33afda23bffd8
sossw1 <sossw1@gmail.com>
2021-04-22 22:02:37 -0400

    Added styles, Sections

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React, {Component} from 'react';
 import M from 'materialize-css';
+import './App.css';
 // Components
 import Navbar from './components/Navbar';
 import Sidenav from './components/Sidenav';
 import Header from './components/Header';
+import Section from './components/Section';
 
 class App extends Component {
   componentDidMount() {
@@ -16,6 +18,21 @@ class App extends Component {
         <Navbar />
         <Sidenav />
         <Header />
+        <Section id='profile' title='Profile' color='cyan'>
+          
+        </Section>
+        <Section id='experience' title='Experience' color='teal'>
+          
+        </Section>
+        <Section id='technology' title='Technology' color='green'>
+          
+        </Section>
+        <Section id='education' title='Education' color='amber'>
+          
+        </Section>
+        <Section id='projects' title='Projects' color='blue'>
+          
+        </Section>
       </>
     );
   }
