78076edc2313a41af78996d688e16751918c688e
sossw1 <sossw1@gmail.com>
2021-04-22 20:38:17 -0400

    Navbar component

---
+++ src/components/Navbar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Navbar () {
+  return (
+    <nav>
+      <div className="nav-wrapper blue-grey darken-4">
+        <a href="#nav" data-target="slide-out" className="sidenav-trigger">
+          <i className="material-icons">menu</i>
+        </a>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
