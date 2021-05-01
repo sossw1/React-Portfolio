4615fd236a04e634cdf25da62f52bd93a58a2a70
sossw1 <sossw1@gmail.com>
2021-04-30 20:11:43 -0400

    home id for navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Navbar() {
   return (
     <nav>
-      <div className='nav-wrapper blue-grey darken-4'>
+      <div id='home' className='nav-wrapper blue-grey darken-4'>
         <a href='#nav' data-target='slide-out' className='sidenav-trigger'>
           <i className='material-icons'>menu</i>
         </a>
