dfc780466e2be9303082553d59f400e0046b3658
sossw1 <sossw1@gmail.com>
2021-04-30 20:01:53 -0400

    Added header tag

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,13 +3,15 @@ import headerImage from '../images/tech-background.jpg';
 
 function Header() {
   return (
-    <div className='parallax-container'>
-      <div className='parallax'>
-        <img src={headerImage} alt='Tech background' />
+    <header>
+      <div className='parallax-container'>
+        <div className='parallax'>
+          <img src={headerImage} alt='Tech background' />
+        </div>
+        <h1 className='center-align white-text'>William Soss</h1>
+        <h4 className='center-align white-text'>Full Stack Web Developer</h4>
       </div>
-      <h1 className='center-align white-text'>William Soss</h1>
-      <h4 className='center-align white-text'>Full Stack Web Developer</h4>
-    </div>
+    </header>
   );
 }
 
