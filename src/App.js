4f3208b74b009b2cf1f77e3738ca8026ce0c469d
sossw1 <sossw1@gmail.com>
2021-04-30 20:01:35 -0400

    Added main tag wrapper

--- src/App.js
+++ src/App.js
@@ -26,45 +26,47 @@ class App extends Component {
         <Navbar />
         <Sidenav />
         <Header />
-        <Section id='profile' title='Profile' color='cyan'>
-          <ProfilePicture />
-          <ProfileInfo />
-        </Section>
-        {/* <Section id='experience' title='Experience' color='teal'>
-          
-        </Section>
-        <Section id='technology' title='Technology' color='green'>
-          
-        </Section>
-        <Section id='education' title='Education' color='amber'>
-          
-        </Section> */}
-        <Section id='projects' title='Projects' color='blue'>
-          <ProjectCard
-            color='blue' title='Takeoff' screenshot={takeoffScreenshot}
-            repositoryLink='https://github.com/sossw1/Takeoff'
-            appLink='https://takeoff123.herokuapp.com/'
-            description='A travel planning and blogging app that brings your 
-            past and future trips together in one place!' />
-          <ProjectCard
-            color='blue' title='Purple Planet' screenshot={purpleScreenshot}
-            repositoryLink='https://github.com/sossw1/project-purple-planet'
-            appLink='https://purple-planet.herokuapp.com/'
-            description= 'A simple, clean, lightweight and free personal \
-            project and task manager!' />
-          <ProjectCard
-            color='blue' title='My Cool Playlist' screenshot={playlistScreenshot}
-            repositoryLink='https://github.com/sossw1/playlist-creator'
-            appLink='https://sossw1.github.io/Playlist-Creator/'
-            description='A free, private music app for creating playlists and \
-            viewing song data!' />
-          <ProjectCard
-            color='blue' title='Weather Dashboard' screenshot={weatherScreenshot}
-            repositoryLink='https://github.com/sossw1/Weather-Dashboard'
-            appLink='https://sossw1.github.io/Weather-Dashboard/'
-            description='A quick, easy weather app that displays current \
-            weather and a 5-day forecast!' />
-        </Section>
+        <main>
+          <Section id='profile' title='Profile' color='cyan'>
+            <ProfilePicture />
+            <ProfileInfo />
+          </Section>
+          {/* <Section id='experience' title='Experience' color='teal'>
+            
+          </Section>
+          <Section id='technology' title='Technology' color='green'>
+            
+          </Section>
+          <Section id='education' title='Education' color='amber'>
+            
+          </Section> */}
+          <Section id='projects' title='Projects' color='blue'>
+            <ProjectCard
+              color='blue' title='Takeoff' screenshot={takeoffScreenshot}
+              repositoryLink='https://github.com/sossw1/Takeoff'
+              appLink='https://takeoff123.herokuapp.com/'
+              description='A travel planning and blogging app that brings your 
+              past and future trips together in one place!' />
+            <ProjectCard
+              color='blue' title='Purple Planet' screenshot={purpleScreenshot}
+              repositoryLink='https://github.com/sossw1/project-purple-planet'
+              appLink='https://purple-planet.herokuapp.com/'
+              description= 'A simple, clean, lightweight and free personal \
+              project and task manager!' />
+            <ProjectCard
+              color='blue' title='My Cool Playlist' screenshot={playlistScreenshot}
+              repositoryLink='https://github.com/sossw1/playlist-creator'
+              appLink='https://sossw1.github.io/Playlist-Creator/'
+              description='A free, private music app for creating playlists and \
+              viewing song data!' />
+            <ProjectCard
+              color='blue' title='Weather Dashboard' screenshot={weatherScreenshot}
+              repositoryLink='https://github.com/sossw1/Weather-Dashboard'
+              appLink='https://sossw1.github.io/Weather-Dashboard/'
+              description='A quick, easy weather app that displays current \
+              weather and a 5-day forecast!' />
+          </Section>
+        </main>
       </>
     );
   }
