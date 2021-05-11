a053caef0cef22264040f62791c9b1c08771abe6
sossw1 <sossw1@gmail.com>
2021-05-10 21:48:18 -0400

    Education section link

--- src/components/Sidenav.js
+++ src/components/Sidenav.js
@@ -9,17 +9,14 @@ function Sidenav() {
       <li>
         <a href='#profile' className='white-text sidenav-close'><i className='material-icons white-text'>assignment_ind</i>Profile</a>
       </li>
-      {/* <li>
-        <a href='#experience' className='white-text sidenav-close'><i className='material-icons white-text'>location_city</i>Experience</a>
-      </li>
-      <li>
-        <a href='#education' className='white-text sidenav-close'><i className='material-icons white-text'>school</i>Education</a>
-      </li> */}
       <li>
         <a href='#projects' className='white-text sidenav-close'><i className='material-icons white-text'>wb_incandescent</i>Projects</a>
       </li>
       <li>
         <a href='#technology' className='white-text sidenav-close'><i className='material-icons white-text'>personal_video</i>Technology</a>
+      </li>
+      <li>
+        <a href='#education' className='white-text sidenav-close'><i className='material-icons white-text'>school</i>Education</a>
       </li>
     </ul>
   );
