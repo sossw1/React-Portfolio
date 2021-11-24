9645b59a4b0eae113f023b1a23a3ea781209691b
sossw1 <sossw1@gmail.com>
2021-11-24 11:06:50 -0500

    MUI theme

---
+++ src/theme.js
@@ -0,0 +1,18 @@
+import { createTheme } from '@mui/material/styles';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      light: '#33BCCD',
+      main: '#00ACC1',
+      dark: '#007887'
+    },
+    secondary: {
+      light: '#CD4333',
+      main: '#C11500',
+      dark: '#870e00'
+    },
+  },
+});
+
+export default theme;
