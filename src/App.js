237b1d60655c4a2bf1cfd770da5bbe724dcecc78
sossw1 <sossw1@gmail.com>
2021-11-24 14:42:29 -0500

    pass toggleDrawer to TopBar

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ class App extends Component {
 
     return (
       <ThemeProvider theme={theme}>
-        <TopBar />
+        <TopBar toggleDrawer={this.toggleDrawer} />
       </ThemeProvider>
     );
   }
