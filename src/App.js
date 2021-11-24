cf6e8169b47ae99514dc47798b46bdcc8ec4b417
sossw1 <sossw1@gmail.com>
2021-11-24 14:40:07 -0500

    initial state

--- src/App.js
+++ src/App.js
@@ -6,6 +6,13 @@ import theme from './theme';
 import TopBar from './components/TopBar';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isDrawerOpen: false
+    }
+  }
+
   render() {
     return (
       <ThemeProvider theme={theme}>
