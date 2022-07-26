b2878dd6316975fe5dba81aabf9112351c1ee1c7
sossw1 <sossw1@gmail.com>
2022-07-25 21:04:10 -0400

    primary and secondary colors

--- src/Theme.tsx
+++ src/Theme.tsx
@@ -1,5 +1,14 @@
 import { createTheme } from '@mui/material/styles';
 
-const theme = createTheme({});
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#00e5ff'
+    },
+    secondary: {
+      main: '#ffc107'
+    }
+  }
+});
 
 export default theme;
