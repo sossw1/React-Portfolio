0b428aa6faca3f06a4b5bde3cf53d747d3546260
sossw1 <sossw1@gmail.com>
2022-07-27 19:51:18 -0400

    section background color

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
+import { Box } from '@mui/material';
 import { ThemeProvider } from '@mui/material/styles';
-import Intro from './components/Intro';
 import theme from './Theme';
+import Intro from './components/Intro';
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
-      <Intro />
+      <Box sx={{ backgroundColor: theme.palette.grey[300] }}>
+        <Intro />
+      </Box>
     </ThemeProvider>
   );
 }
