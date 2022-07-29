60cd9a1c873d260342e4255762e61636eb2c65be
sossw1 <sossw1@gmail.com>
2022-07-29 16:39:30 -0400

    background box on app level

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import { ThemeProvider } from '@mui/material/styles';
+import { Box } from '@mui/material';
 import theme from './Theme';
 import Intro from './components/Intro';
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
-      <Intro />
+      <Box
+        sx={{ backgroundColor: theme.palette.grey[300], minHeight: '100vh' }}
+      >
+        <Intro />
+      </Box>
     </ThemeProvider>
   );
 }
--- src/components/Intro.tsx
+++ src/components/Intro.tsx
@@ -52,128 +52,126 @@ export default function Intro() {
   }, [audio]);
 
   return (
-    <Box sx={{ backgroundColor: theme.palette.grey[300] }}>
-      <Box display='flex' flexDirection='column' alignItems='center'>
-        <Box
-          mt='5rem'
-          pb='1rem'
-          display='flex'
-          flexDirection='row'
-          sx={{ overflow: 'hidden', animation: `2s ${lineUp} ease-out` }}
-        >
-          <Box>
-            <Typography
-              variant='h3'
-              pb='1.5rem'
-              pr='1.5rem'
-              sx={{
-                animation: `${upAndRotation} 0.5s linear 2`,
-                animationDelay: '1s'
-              }}
-            >
-              👋
-            </Typography>
-          </Box>
-          <Box>
-            <Typography
-              variant='h3'
-              display='inline'
-              component='p'
-              mb='1rem'
-              sx={{
-                ...theme.narration
-              }}
-            >
-              Hi, I'm Bill Soss
-            </Typography>
-          </Box>
-        </Box>
-        <Paper
-          component='section'
-          sx={{ m: '5rem 0', p: '5rem', borderRadius: '10px' }}
-        >
+    <Box display='flex' flexDirection='column' alignItems='center'>
+      <Box
+        mt='5rem'
+        pb='1rem'
+        display='flex'
+        flexDirection='row'
+        sx={{ overflow: 'hidden', animation: `2s ${lineUp} ease-out` }}
+      >
+        <Box>
           <Typography
-            variant='h1'
-            fontFamily='Playfair Display,serif'
-            fontWeight='bold'
+            variant='h3'
+            pb='1.5rem'
+            pr='1.5rem'
+            sx={{
+              animation: `${upAndRotation} 0.5s linear 2`,
+              animationDelay: '1s'
+            }}
           >
-            bill soss{' '}
-            <Typography
-              variant='h3'
-              component='span'
-              display='inline'
-              color='primary'
-              fontFamily='Playfair Display,serif'
-            >
-              noun (1)
-            </Typography>
+            👋
           </Typography>
+        </Box>
+        <Box>
+          <Typography
+            variant='h3'
+            display='inline'
+            component='p'
+            mb='1rem'
+            sx={{
+              ...theme.narration
+            }}
+          >
+            Hi, I'm Bill Soss
+          </Typography>
+        </Box>
+      </Box>
+      <Paper
+        component='section'
+        sx={{ m: '5rem 0', p: '5rem', borderRadius: '10px' }}
+      >
+        <Typography
+          variant='h1'
+          fontFamily='Playfair Display,serif'
+          fontWeight='bold'
+        >
+          bill soss{' '}
+          <Typography
+            variant='h3'
+            component='span'
+            display='inline'
+            color='primary'
+            fontFamily='Playfair Display,serif'
+          >
+            noun (1)
+          </Typography>
+        </Typography>
+        <Typography
+          variant='h5'
+          component='p'
+          mb='0.75rem'
+          sx={{ ...theme.dictionary }}
+        >
+          bill·soss
           <Typography
             variant='h5'
-            component='p'
-            mb='0.75rem'
+            component='span'
+            p='0 1.5rem'
+            fontWeight='bold'
+            display='inline'
             sx={{ ...theme.dictionary }}
           >
-            bill·soss
+            |
+          </Typography>
+          \ 'bil sȯs{' '}
+          <Button
+            sx={{
+              display: 'inline',
+              lineHeight: '1rem',
+              p: '0 5px',
+              minWidth: '24px'
+            }}
+            onClick={togglePlaying}
+          >
+            <VolumeUp />
+          </Button>{' '}
+          \
+        </Typography>
+        <Typography
+          variant='h4'
+          component='h2'
+          mb='1rem'
+          sx={{ ...theme.dictionary }}
+        >
+          Definition of <i>bill soss</i> (Entry 1 of 1)
+        </Typography>
+        {[
+          'full-stack web developer',
+          '9-ball billiards team captain',
+          'pc and board game enthusiast'
+        ].map((item, index) => (
+          <Typography
+            key={uuid()}
+            variant='h5'
+            component='p'
+            mb='0.5rem'
+            sx={{ fontWeight: '500', ...theme.dictionary }}
+          >
             <Typography
               variant='h5'
               component='span'
-              p='0 1.5rem'
-              fontWeight='bold'
               display='inline'
+              fontWeight='bold'
+              pr='1rem'
               sx={{ ...theme.dictionary }}
             >
-              |
+              {index + 1} :
             </Typography>
-            \ 'bil sȯs{' '}
-            <Button
-              sx={{
-                display: 'inline',
-                lineHeight: '1rem',
-                p: '0 5px',
-                minWidth: '24px'
-              }}
-              onClick={togglePlaying}
-            >
-              <VolumeUp />
-            </Button>{' '}
-            \
+            {item}
           </Typography>
-          <Typography
-            variant='h4'
-            component='h2'
-            mb='1rem'
-            sx={{ ...theme.dictionary }}
-          >
-            Definition of <i>bill soss</i> (Entry 1 of 1)
-          </Typography>
-          {[
-            'full-stack web developer',
-            '9-ball billiards team captain',
-            'pc and board game enthusiast'
-          ].map((item, index) => (
-            <Typography
-              key={uuid()}
-              variant='h5'
-              component='p'
-              mb='0.5rem'
-              sx={{ fontWeight: '500', ...theme.dictionary }}
-            >
-              <Typography
-                variant='h5'
-                component='span'
-                display='inline'
-                fontWeight='bold'
-                pr='1rem'
-                sx={{ ...theme.dictionary }}
-              >
-                {index + 1} :
-              </Typography>
-              {item}
-            </Typography>
-          ))}
-        </Paper>
-      </Box>
+        ))}
+      </Paper>
     </Box>
   );
 }
