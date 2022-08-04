617c4d24e3c0df30c42ea7be763f42d376d31d3f
sossw1 <sossw1@gmail.com>
2022-08-04 12:06:11 -0400

    logo cards

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -1,19 +1,34 @@
-import { Box, Button, Icon, Typography } from '@mui/material';
+import { Box, Button, Icon, Paper, Typography } from '@mui/material';
 import { ChevronLeft, ChevronRight } from '@mui/icons-material';
+import { v4 as uuid } from 'uuid';
 import theme from '../Theme';
+
+const logos = [
+  { name: 'HTML', path: '/images/HTML.png' },
+  { name: 'CSS', path: '/images/CSS.png' },
+  { name: 'JavaScript', path: '/images/JavaScript.png' },
+  { name: 'TypeScript', path: '/images/TypeScript.png' },
+  { name: 'Node.js', path: '/images/NodeJS.png' },
+  { name: 'MongoDB', path: '/images/MongoDB.png' },
+  { name: 'MySQL', path: '/images/MySQL.png' },
+  { name: 'React', path: '/images/React.png' },
+  { name: 'Mui', path: '/images/Mui.png' },
+  { name: 'Jest', path: '/images/Jest.png' }
+];
 
 export default function Skills() {
   return (
-    <Box>
+    <Box mb='5rem'>
       <Typography
         variant='h3'
         component='h2'
         mb='2rem'
         sx={{ ...theme.narration }}
+        textAlign='center'
       >
         Skills
       </Typography>
-      <Box display='flex' flexDirection='row' pb='5rem'>
+      <Box display='flex' flexDirection='row'>
         <Button
           variant='contained'
           disableRipple
@@ -28,7 +43,41 @@ export default function Skills() {
             <ChevronLeft />
           </Icon>
         </Button>
-        <Box p='0 0.5rem' width='100%'></Box>
+        <Box
+          m='0 0.5rem'
+          maxWidth='70vw'
+          display='flex'
+          flexDirection='row'
+          sx={{ overflowX: 'hidden' }}
+        >
+          {logos.map((logo) => (
+            <Paper
+              key={uuid()}
+              sx={{
+                m: '0 0.5rem',
+                p: '0.5rem',
+                display: 'flex',
+                flexDirection: 'column'
+              }}
+            >
+              <Box
+                component='img'
+                src={logo.path}
+                width='100px'
+                height='100px'
+                m='0 1rem 1rem'
+              ></Box>
+              <Typography
+                variant='h6'
+                component='p'
+                textAlign='center'
+                sx={{ ...theme.dictionary }}
+              >
+                {logo.name}
+              </Typography>
+            </Paper>
+          ))}
+        </Box>
         <Button
           variant='contained'
           disableRipple
