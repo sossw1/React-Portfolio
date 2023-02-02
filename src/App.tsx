c0d04ab0a2127043180ac37adc338b4fff1b6ffc
sossw1 <sossw1@gmail.com>
2023-02-01 22:40:57 -0500

    lighten grey background

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ function App() {
     <ThemeProvider theme={theme}>
       <Box
         sx={{
-          backgroundColor: theme.palette.grey[300],
+          backgroundColor: theme.palette.grey[200],
           minHeight: 'calc(100vh + 1px)'
         }}
       >
